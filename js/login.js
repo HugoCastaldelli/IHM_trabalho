a5e7177d81e2dc143d957e212785f8fe200c77e4
Gabriel Henrique Franco Vieira <102386109+AscendantZeroOne@users.noreply.github.com>
2025-11-17 23:51:11 -0300

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -1,3 +1,21 @@
+(function preloadUser() {
+    const users = JSON.parse(localStorage.getItem("users") || "{}");
+
+    const defaultEmail = "rogerio@gmail.com";
+
+    if (!users[defaultEmail]) {
+        users[defaultEmail] = {
+            nome: "Rogerio",
+            senha: "roger"
+        };
+
+        localStorage.setItem("users", JSON.stringify(users));
+        console.log("Usuário padrão 'Rogerio' criado.");
+    }
+})();
+
+
+
 function login() {
     const entrada = document.getElementById("email").value.trim();
     const senha = document.getElementById("senha").value.trim();
@@ -24,6 +42,7 @@ function login() {
 
     alert("Usuário ou senha incorretos.");
 }
+
 
 function social(provider) {
     const tempPassword = Math.random().toString(36).substring(2, 10);
